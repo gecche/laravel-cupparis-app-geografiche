805596403b0583ee009a0b504ec0a1b90597e14b
Giacomo Terreni <giacomo.terreni@gmail.com>
2020-10-30 13:14:21 +0100

    descrizione - nome_it

--- public/admin/ModelConfs/CupGeoArea.js
+++ public/admin/ModelConfs/CupGeoArea.js
@@ -3,7 +3,7 @@ var CupGeoArea = {
     search: {
         modelName : 'cup_geo_area',
         //langContext : 'user',
-        fields : ['descrizione'],
+        fields : ['nome_it'],
     },
     // view : {
     //     modelName : 'cup_geo_area',
@@ -18,17 +18,17 @@ var CupGeoArea = {
     // },
     list: {
         modelName : 'cup_geo_area',
-        fields : ['codice','descrizione'],
+        fields : ['codice','nome_it'],
         actions : ['action-edit','action-delete','action-insert'],
         orderFields : {
             'codice':'codice',
-            'descrizione':'descrizione'
+            'nome_it':'nome_it'
         },
     },
     edit: {
         modelName : 'cup_geo_area',
         actions : ['action-save','action-back'],
-        fields : ['codice','descrizione',
+        fields : ['codice','nome_it',
             //'comuni'
         ],
     },
